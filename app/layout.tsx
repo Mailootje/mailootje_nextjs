2a3e71e74020bb421c81d057f8a33ea1b7aee38d
Mailo <oliam2002@hotmail.com>
2025-11-24 22:37:20 +0100

    Changed Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-    title: "Mailo .purple",
+    title: "Mailootje",
     description: "Personal site inspired by eva.pink, themed in purple.",
 };
 
