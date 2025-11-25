a58da60e9eff20d33b5303a44adcf54d2a7fe0d2
Mailo <oliam2002@hotmail.com>
2025-11-25 15:56:50 +0100

    Fixed the spacing for mobile (it was overflowing)

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             </header>
 
             {/* Responsive eva-style layout */}
-            <section className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            <section className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                 {/* ---------------- LEFT SIDE ---------------- */}
                 <div className="md:col-span-2 lg:col-span-2 grid gap-6">
                     {/* PROFILE */}
@@ -203,7 +203,7 @@ export default function Home() {
                 </div>
 
                 {/* ---------------- RIGHT SIDE STACK ---------------- */}
-                <div className="md:col-span-2 lg:col-start-3 flex flex-col gap-6">
+                <div className="md:col-span-2 lg:col-span-1 lg:col-start-3 flex flex-col gap-6">
                     {/* WEATHER */}
                     <WeatherCard />
 
